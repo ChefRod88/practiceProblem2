ac9aead5fd25c9216ac518cb09a9917b619a7b1a
Rodney Chery <rchery7@wgu.edu>
2024-12-28 10:24:46 -0500

    Initial commit

---
+++ practiceProblem2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Accepting input for the number of ounces
+        Console.Write("Enter the number of ounces: ");
+        int ounces = int.Parse(Console.ReadLine());
+
+        // Defining constants for conversion
+        int ouncesInPound = 16;
+        int ouncesInTon = 32000;
+
+        // Calculating tons
+        int tons = ounces / ouncesInTon;
+        int remainingOunces = ounces % ouncesInTon;
+
+        // Calculating pounds
+        int pounds = remainingOunces / ouncesInPound;
+
+        // Calculating remaining ounces
+        remainingOunces = remainingOunces % ouncesInPound;
+
+        // Outputting the results
+        Console.WriteLine($"Tons: {tons}");
+        Console.WriteLine($"Pounds: {pounds}");
+        Console.WriteLine($"Ounces: {remainingOunces}");
+    }
+}
